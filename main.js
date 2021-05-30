5a915f860f9a70ddfa40cd435a25917cf4d881fd
AryehSchoefer <aryeh.shmuel@googlemail.com>
2021-05-30 23:38:15 +0200

    Initial commit: working prototype

--- main.js
+++ main.js
@@ -1,43 +1,76 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow } = require('electron')
+const { ipcMain } = require('electron/main')
+const fs = require('fs')
 const path = require('path')
 
-function createWindow () {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+// hot-reload, buggy as shit (might heavily influence functionality of code)
+// try {
+// 	require('electron-reloader')(module)
+// } catch (_) {}
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+function createWindow() {
+	// Create the browser window.
+	const mainWindow = new BrowserWindow({
+		width: 800,
+		height: 800,
+		webPreferences: {
+			preload: path.join(__dirname, 'preload.js'),
+			nodeIntegration: true,
+			contextIsolation: false,
+		},
+	})
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+	// and load the index.html of the app.
+	mainWindow.loadFile('index.html')
+
+	// Open the DevTools.
+	process.env.NODE_ENV !== 'production' && mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow()
-  
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
+	createWindow()
+
+	app.on('activate', function () {
+		// On macOS it's common to re-create a window in the app when the
+		// dock icon is clicked and there are no other windows open.
+		if (BrowserWindow.getAllWindows().length === 0) createWindow()
+	})
 })
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+	if (process.platform !== 'darwin') app.quit()
 })
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+ipcMain.on('userinput', (event, args) => {
+	const [msg, Key] = args
+	console.log(`KEY: ${Key}`)
+	switch (msg) {
+		case 'load-key':
+			const rawdata = fs.readFileSync('config.json')
+			const { key: LoadedKey } = JSON.parse(rawdata)
+			console.log(`msg: ${msg}, key: ${LoadedKey}`)
+			break
+		case 'save-key':
+			if (!Key) return
+			console.log(`msg: ${msg}, key: ${Key}`)
+			fs.writeFileSync('config.json', JSON.stringify({ key: `${Key}` }))
+			break
+		case 'get-wallpaper':
+			console.log(msg)
+			break
+		default:
+			break
+	}
+})
+
+// Key: 563492ad6f91700001000001f6fab3378c474797aeb1f944160d1630
--- renderer.js
+++ renderer.js
@@ -1,6 +1,84 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
+const { ipcRenderer } = require('electron/renderer')
+const wallpaper = require('wallpaper')
+const fs = require('fs')
+const path = require('path')
+
+const apikeyInput = document.querySelector('#api-key')
+const loadKeyBtn = document.querySelector('#load-key')
+const saveKeyBtn = document.querySelector('#save-key')
+const imgElem = document.querySelector('#wallpaper')
+const searchInput = document.querySelector('#search-query')
+const getWallpaperBtn = document.querySelector('#get-wallpaper')
+const wallpaperObj = {
+	url: 'https://images.pexels.com/photos/1056251/pexels-photo-1056251.jpeg',
+	width: 5184,
+	height: 3456,
+}
+
+loadKeyBtn.addEventListener('click', () => {
+	ipcRenderer.send('userinput', ['load-key'])
+})
+
+saveKeyBtn.addEventListener('click', () => {
+	ipcRenderer.send('userinput', ['save-key', apikeyInput.value])
+})
+
+imgElem.addEventListener('click', (event) => {
+	setAsWallpaper(event)
+})
+
+getWallpaperBtn.addEventListener('click', () => {
+	ipcRenderer.send('userinput', ['get-wallpaper']) // this might be useless
+	getWallpaperBtn.textContent = 'Loading..'
+
+	setWallpaper(searchInput.value)
+})
+
+function convertToBase64(img) {
+	const canvas = document.createElement('canvas')
+	canvas.width = wallpaperObj.width
+	canvas.height = wallpaperObj.height
+	const context = canvas.getContext('2d')
+	context.drawImage(img, 0, 0)
+	const dataURL = canvas.toDataURL('image/jpeg')
+	return dataURL.replace(/^data:image\/(png|jpg|jpeg);base64,/, '')
+}
+
+function setAsWallpaper(event) {
+	const base64Image = convertToBase64(event.target)
+	let picturePath = path.join(__dirname, 'recent.jpeg')
+	picturePath = path.normalize(picturePath)
+	fs.writeFile(picturePath, base64Image, 'base64', async (err) => {
+		console.error(err)
+		await wallpaper.set(picturePath, { scale: 'stretch' }).then(() => {
+			console.log(path.resolve(picturePath))
+		})
+	})
+}
+
+async function setWallpaper(query) {
+	API_ENDPOINT = 'https://api.pexels.com/v1/search'
+	const rawdata = fs.readFileSync('config.json')
+	const { key: API_KEY } = JSON.parse(rawdata)
+
+	const response = await fetch(`${API_ENDPOINT}?query=${query}`, {
+		headers: {
+			Authorization: `${API_KEY}`,
+		},
+	})
+	const { photos } = await response.json()
+
+	const photoIndex = randomInteger(photos.length)
+	const wallpaperURL = photos[photoIndex].src.original
+	const { width, height } = photos[photoIndex]
+	imgElem.src = wallpaperURL
+	wallpaperObj.url = wallpaperURL
+	wallpaperObj.width = width
+	wallpaperObj.height = height
+
+	getWallpaperBtn.textContent = 'Get'
+}
+
+function randomInteger(max) {
+	return Math.floor(Math.random() * max)
+}
